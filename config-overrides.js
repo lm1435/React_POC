580ae7800c1fd5ca49803921de2119b891b2f2c5
Luis Meza <luismeza01@gmail.com>
2019-08-15 11:20:44 -0500

    updating to yarn lock and fixing one console issue

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,7 @@ const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
 const cspConfigPolicy = {
     'default-src': "'self'",
-    'media-src': "'*'",
+    'media-src': "*",
     'img-src': "'self'",
     'object-src': "'none'",
     'script-src': "'self'",
