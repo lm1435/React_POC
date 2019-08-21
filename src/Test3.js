3b9102847e91c565a8d6d5ad6b745dc6f856b18a
Luis Meza <luismeza01@gmail.com>
2019-08-21 08:39:54 -0500

    updating error for state in const; adding a test svc for axios.

--- src/Test3.js
+++ src/Test3.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import fetchApi from './components/Services/test-svc';
 
-const Test3 = () => (
-  <div className="App">
-    <h1>imported with Loadable....WHHHHATTTT?!</h1>
-  </div>
-);
+class Test3 extends Component {
+  state = {
+    test: null,
+  }
+
+  componentDidMount() {
+    this.response();
+  }
+
+  response = async () => {
+    fetchApi().then((res) => this.setState({
+      test: res.data.results,
+    }));
+  }
+
+  render() {
+    const { test } = this.state;
+    return (
+      <>
+        { test ? (
+          <div className="App">
+            {test.map((item) => (
+              <div>
+                {item.title}
+                <br />
+              </div>
+            ))}
+            <br />
+            <h1>imported with Loadable....WHHHHATTTT?!</h1>
+          </div>
+        ) : null }
+      </>
+    );
+  }
+}
 
 export default Test3;
---
+++ src/components/Services/test-svc.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default function fetchApi() {
+  return axios.get('https://api.themoviedb.org/3/discover/movie?api_key=9725571b96179202ebd3830a5ee14d01&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+}
