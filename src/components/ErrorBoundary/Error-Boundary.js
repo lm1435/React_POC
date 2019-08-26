20110531753d758cd14a5ee38310b195fb2fba68
Luis Meza <luismeza01@gmail.com>
2019-08-26 14:22:33 -0500

    updating children.

--- src/components/ErrorBoundary/Error-Boundary.js
+++ src/components/ErrorBoundary/Error-Boundary.js
@@ -43,9 +43,7 @@ export default class ErrorBoundary extends Component {
     }
     // Normally, just render children
     return (
-      <>
-        { children }
-      </>
+      { children }
     );
   }
 }
